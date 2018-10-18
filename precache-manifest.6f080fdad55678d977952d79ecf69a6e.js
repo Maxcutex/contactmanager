59f11041bfb59c567fa1d1a6ba04c9879a7da5f8
Eno Bassey <ennyboy@gmail.com>
2018-10-18 18:16:41 +0100

    Updates

---
+++ precache-manifest.6f080fdad55678d977952d79ecf69a6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "af0086967b597326c352",
+    "url": "/contactmanager/static/js/main.af008696.chunk.js"
+  },
+  {
+    "revision": "db3ed80d8952fe0fa17e",
+    "url": "/contactmanager/static/js/1.db3ed80d.chunk.js"
+  },
+  {
+    "revision": "af0086967b597326c352",
+    "url": "/contactmanager/static/css/main.84cd0853.chunk.css"
+  },
+  {
+    "revision": "db3ed80d8952fe0fa17e",
+    "url": "/contactmanager/static/css/1.6bf25aa1.chunk.css"
+  },
+  {
+    "revision": "35c6ffc377d0ec827b01407b00d30da4",
+    "url": "/contactmanager/index.html"
+  }
+];
